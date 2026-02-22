1e1e2e49654fe12bd23d1f94fa867a52d7d88af4
s3cr3tmod-xp <s3cr3tmod@gmail.com>
2026-02-22 13:05:19 +0700

    Update blight.py

--- blight.py
+++ blight.py
@@ -86,7 +86,7 @@ while True:
             sys.exit("Invalid Number Of Packets!")
         u = udp(ip,port,size,packets)
         u.start()
-        print(f"\033[38;5;220mProtocol-serv |\033[38;5;37m " +(ip)+ " \033[38;5;220m| \033[38;5;111m" +str(self)+ "") 
+        print(f"\033[38;5;220mProtocol-serv |\033[38;5;37m " +(ip)+ " \033[38;5;220m| \033[38;5;111m" +str(randrange)+ "") 
     except KeyboardInterrupt:
         print ("Stopping Flood!")
         sys.exit()
